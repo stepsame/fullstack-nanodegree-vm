f3ec99fad12553ec80a257f6c340b32f6df2bc84
stepsame <stepsame@gmail.com>
2015-10-11 10:21:05 +0800

    final project adding routes

--- vagrant/ud088/finalProject.py
+++ vagrant/ud088/finalProject.py
@@ -2,6 +2,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/restaurant')
+def showRestaurants():
+	return "This page will show all my restaurants"
+
+@app.route('/restaurant/new')
+def newRestaurant():
+	return "This page will be for making a new restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def editRestaurant():
+	return "This page will be for editing restaurant %s" % restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def deleteRestaurant():
+	return "This page will be for deleting restaurant %s" % restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def showMenu():
+	return "This page is the menu for restaurant %s" restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/new')
+def newMenuItem():
+	return "This page is for making a new menu item for restaurant %s" % restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem():
+	return "This page is for editing menu item %s" % menu_id
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem():
+	return "This page is for deleting menu item %s" % menu_id
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host = '0.0.0.0', port = 5000)
